be998f23e09ea8167937eb8ad7d0708ca4fb20cf
Ksusha <ksusha_bor@ukr.net>
2019-10-19 10:49:13 +0300

    Revert "Initial commit"
    This reverts commit ecdddcb08a9d3caa1ad1935a73b3c72fe8fb2cdd.
    
    # Conflicts:
    #	.idea/vcs.xml
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,7 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
